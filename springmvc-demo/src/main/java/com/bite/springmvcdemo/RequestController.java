9822004600bb2ae357dccafc739393459562555b
李承轩 <1206817338@qq.com>
2026-01-16 00:27:15 +0800

    SpringMVC学习

---
+++ springmvc-demo/src/main/java/com/bite/springmvcdemo/RequestController.java
@@ -0,0 +1,5 @@
+package com.bite.springmvcdemo;
+
+public class RequestController {
+    
+}
---
+++ springmvc-demo/src/test/java/com/bite/springmvcdemo/SpringmvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bite.springmvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
