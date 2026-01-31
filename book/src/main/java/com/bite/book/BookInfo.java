f2ae7289b6c07a311e9b8a686cc2cf0e850382b8
李承轩 <1206817338@qq.com>
2026-02-01 00:38:08 +0800

    图书管理系统

---
+++ book/src/main/java/com/bite/book/BookInfo.java
@@ -0,0 +1,17 @@
+package com.bite.book;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class BookInfo {
+    private Integer id;
+    private String bookName;
+    private String author;
+    private Integer num;
+    private BigDecimal price;
+    private String publishName;
+    private Integer status; //1-正常，2-不可借阅
+    private String StatusCN;
+}
---
+++ book/src/main/java/com/bite/book/UserController.java
@@ -0,0 +1,28 @@
+package com.bite.book;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+    @RequestMapping("/login")
+    public String login(String userName, String password){
+        //1.校验参数
+        if(!StringUtils.hasLength(userName)||!StringUtils.hasLength(password)){
+            return "用户名或者密码为空";
+        }
+        //模拟校验
+//        if("admin".equals(userName)&&"admin".equals(password)){
+//            return "";
+//        }
+//        return "密码错误";
+        if(!"admin".equals(userName) || !"admin".equals(password)){
+            return "密码错误";
+        }
+        return "";
+        //2.验证密码是否正确
+        //3.返回相应结果
+    }
+}
