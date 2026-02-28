22cd56523b01faee8066eaa89cb8418ee809a5cb
李承轩 <1206817338@qq.com>
2026-02-28 16:43:20 +0800

    图书管理系统

---
+++ book/src/main/java/com/bite/book/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.bite.book.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+    @RequestMapping("/t1")
+    public String t1(){
+        int result = 10/0;
+        return "string";
+    }
+
+    @RequestMapping("/t2")
+    public Integer t2(){
+        String str = null;
+        System.out.println(str.length());
+        return 1;
+    }
+
+    @RequestMapping("/t3")
+    public Boolean t3(){
+        Integer[] integers = new Integer[]{1,2,3,4};
+        System.out.println(integers[5]);
+        return true;
+    }
+}
